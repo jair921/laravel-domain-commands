27b8becba7ecb314eebf1de5a8a9e142e79a8311
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2021-03-04 19:47:59 +0300

    Update MakeComponentCommand.php

--- src/Commands/MakeComponentCommand.php
+++ src/Commands/MakeComponentCommand.php
@@ -2,6 +2,7 @@
 
 namespace Notiv\Console\Commands;
 
+use Illuminate\Support\Str;
 use Notiv\Console\Commands\Traits\AppTrait;
 
 class MakeComponentCommand extends \Illuminate\Foundation\Console\ComponentMakeCommand
@@ -18,5 +19,21 @@ class MakeComponentCommand extends \Illuminate\Foundation\Console\ComponentMakeC
     protected function getStub()
     {
         return $this->resolveStubPath('/stubs/view-component.stub');
+    }
+
+    /**
+     * Get the view name relative to the components directory.
+     *
+     * @return string view
+     */
+    protected function getView()
+    {
+        $name = str_replace('\\', '/', $this->getNameInput());
+
+        return collect(explode('/', $name))
+            ->map(function ($part) {
+                return Str::kebab($part);
+            })
+            ->implode('.');
     }
 }
